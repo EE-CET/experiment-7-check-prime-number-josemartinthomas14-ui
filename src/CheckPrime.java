6b599a8e6ef556f7cc68c4699091de0d56870aad
josemartinthomas14 <josemartinthomas14@example.com>
2026-04-06 20:59:18 +0530

    Solve experiment 7

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class CheckPrime {
-   
-        // TODO: Read n
-        // TODO: Check if n is prime
-        // Hint:
-        // 1. If n <= 1, it is Not Prime.
-        // 2. Loop from 2 to sqrt(n). If n is divisible by any number, it is Not Prime.
-        // 3. Otherwise, it is Prime.
-        // TODO: Print "Prime" or "Not Prime"
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            if (n <= 1) {
+                System.out.println("Not Prime");
+            } else {
+                boolean isPrime = true;
+                for (int i = 2; i * i <= n; i++) {
+                    if (n % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                System.out.println(isPrime ? "Prime" : "Not Prime");
+            }
+        }
+        scanner.close();
+    }
 }
